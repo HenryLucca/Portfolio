1ff7dc3ee1f65f098754f8612ee7184a6e03b2df
Henry Lucca <henryluccag@gmail.com>
2023-08-02 15:20:41 -0300

    add theme button and base components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,9 @@ import Anchor from "./Anchor";
 
 export default function Navbar(){
     return (
-        <nav className="flex justify-between items-center border-b p-1">
-            <p className="text-2xl ml-1">Henry Lucca</p>
-            <div className="flex gap-4 mr-1">
+        <nav className="flex justify-between items-center border-b dark:border-zinc-50/25 p-2">
+            <p className="text-xl sm:text-2xl ml-2.5">Henry Lucca</p>
+            <div className="flex gap-4 mr-1.5 md:mr-2.5">
                 <Anchor content="About" href="#about"/>
                 <Anchor content="Projects" href="#projects"/>
                 <Anchor content="Contact" href="#contact"/>
