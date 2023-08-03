7f8201b849022f6971cdc0fe8ddf01643848dfe1
Henry Lucca <henryluccag@gmail.com>
2023-08-03 11:05:31 -0300

    feat: navbar translation

--- ni18n.config.ts
+++ ni18n.config.ts
@@ -3,7 +3,7 @@ import type { Ni18nOptions } from 'ni18n'
 
 export const ni18nConfig: Ni18nOptions = {
   supportedLngs: ['en', 'pt-BR'],
-  ns: ['common', 'about'],
+  ns: ['common', 'about', 'presentation-card', 'navbar'],
   react: {
     useSuspense: false,
   },
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,18 @@
 import Anchor from "./Anchor";
+import { useTranslation } from "react-i18next";
 
 export default function Navbar(){
+    const {t} =  useTranslation("navbar");
     return (
-        <nav className="flex justify-between items-center border-b dark:border-zinc-50/25 p-2">
-            <p className="text-xl sm:text-2xl ml-2.5">Henry Lucca</p>
-            <div className="flex gap-4 mr-1.5 md:mr-2.5">
-                <Anchor content="About" href="#about"/>
-                <Anchor content="Projects" href="#projects"/>
-                <Anchor content="Contact" href="#contact"/>
-            </div>
-        </nav>
+        <>
+            <nav className="flex justify-between items-center border-b dark:border-zinc-50/25 p-2">
+                <p className="text-xl sm:text-2xl ml-2.5">Henry Lucca</p>
+                <div className="flex gap-4 mr-1.5 md:mr-2.5">
+                    <Anchor content={t('about')} href="#about"/>
+                    <Anchor content={t('projects')} href="#projects"/>
+                    <Anchor content={t('contact')} href="#contact"/>
+                </div>
+            </nav>
+        </>
     )
 }
