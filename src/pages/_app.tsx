6d71a964b7843251fd94a5f92008cb7a7e89f30a
Henry Lucca <henryluccag@gmail.com>
2023-09-19 11:52:57 -0300

    feat: add head meta tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { appWithI18Next, useSyncLanguage } from 'ni18n'
 import { ni18nConfig } from '../../ni18n.config'
-import { use } from 'i18next';
 
 function App({ Component, pageProps }: AppProps) {
   // const locale = typeof window !== 'undefined' ? window.location.pathname.split('/')[1] : 'en'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,16 +5,27 @@ import ThemeButton from "@/components/ThemeButton";
 import MainPageContent from "@/components/main-page/MainPageContent";
 import { LanguageSwitch } from "@/components/LanguageSwitch";
 import { useTranslation } from "react-i18next";
-import { GithubIcon, LinkedInIcon } from "@/icons/icons";
+import { Helmet } from "react-helmet";
 
 const playfairDisplay = Playfair_Display({ subsets: ["latin"] });
 
 export default function Home() {
   const [theme, setTheme] = useState(""); // empty string = light theme, 'dark' = dark theme
-  const {t} = useTranslation("common");
+  const { t } = useTranslation("common");
 
   return (
     <div className={`${theme} scroll-smooth ${playfairDisplay.className}`}>
+      <Helmet>
+        <title>Henry Lucca</title>
+        <meta name="description" content="Henry Lucca's personal website" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="author" content="Henry Lucca" />
+        <meta
+          name="keywords"
+          content="Henry Lucca, Lucca, Henry, Henrique, Henrique Lucca, Desenvolvedor, Desenvolvedor Web, Web Developer, Front End, Front End Developer, Full Stack, Full Stack Developer, Desenvolvedor Full Stack, Desenvolvedor Web Full Stack, Web Developer Full Stack, Desenvolvedor Web Full Stack, Desenvolvedor Full Stack, Desenvolvedor Full Stack"
+        />
+      </Helmet>
+
       <div
         className={`bg-white dark:bg-zinc-800 text-zinc-800 dark:text-zinc-100`}
       >
@@ -28,7 +39,6 @@ export default function Home() {
         </div>
 
         <footer className="flex flex-col justify-center items-center text-center">
-          
           <p className="text-xs">{t("made-with")}</p>
           <p className="text-xs">
             © 2023 Henry Lucca - Todos os direitos reservados.
